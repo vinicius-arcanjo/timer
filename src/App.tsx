6c4bc92d9cbd2921a01e80bdca73008bca9f9efe
Vinicius Arcanjo <vinicius@arcanjo.dev>
2023-09-05 03:18:26 -0300

    chore: added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import {ThemeProvider} from "styled-components";
-import {defaultTheme} from "./styles/theme.ts";
-import {GlobalStyle} from "./styles/global.ts";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme.ts'
+import { GlobalStyle } from './styles/global.ts'
 
 function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
